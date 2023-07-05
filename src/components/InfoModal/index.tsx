5d1e9f0a47022d97ead69a5c03079109f8e15f10
Eduardo <eduangelinicarvalho@gmail.com>
2023-07-05 00:40:22 -0300

    commit#2.1-infobutton-fix

--- src/components/InfoModal/index.tsx
+++ src/components/InfoModal/index.tsx
@@ -22,33 +22,33 @@ const InfoModal = (props: InfoModalProps) => {
         <div
           className='h-full w-full flex items-start mt-6 p-3 justify-start text-lg font-bold'
         >
-          <ul className='flex flex-col gap-y-4'>
-            <li>
+          <ul className='flex flex-col gap-y-4 indent-2'>
+            <li className='flex justify-center text-2xl'>
               Como jogar:
             </li>
             <li>
-              1. Após clicar em "Play Now", defina um tema com os seus adversários.
+              Após clicar em "Play Now", defina um tema com os seus adversários.
             </li>
             <li>
-              2. Cada jogador terá que escolher algo relacionada ao tema. Por exemplo: O tema é "Filmes", então o jogador pode escolher "Harry Potter" ou qualquer outro item que se encaixe no tema escolhido.
+              Cada jogador terá que escolher algo relacionada ao tema. Por exemplo: O tema é "Filmes", então o jogador pode escolher "Harry Potter" ou qualquer outro item que se encaixe no tema escolhido.
             </li>
             <li>
-              -&gt; Importante: A quantidade de itens em cada Draft é definido junto com o tema. Por exemplo: O tema é "Filmes" e o Draft é de 5 itens, então cada jogador terá que escolher 5 filmes.
+              Importante: A quantidade de itens em cada Draft é definido junto com o tema. Por exemplo: O tema é "Filmes" e o Draft é de 5 itens, então cada jogador terá que escolher 5 filmes.
             </li>
             <li>
-              -&gt; Importante: Os jogadores escolhem na ordem crescente (do jogador 1 ao jogador 5), o último jogador faz duas escolhas e as escolhas voltam em ordem descrecente(do jogador 5 ao jogador 1).
+              Importante: Os jogadores escolhem na ordem crescente (do jogador 1 ao jogador 5), o último jogador faz duas escolhas e as escolhas voltam em ordem descrecente(do jogador 5 ao jogador 1).
             </li>
             <li>
-              3. Após todos os jogadores terem o seu Draft montado é a hora da votação. Só pode votar quem nao participou do Draft, adicione ao contador do Draft cada vez que o mesmo receber um voto e depois que todos votarem, o jogador com mais votos ganha.
+              Após todos os jogadores terem o seu Draft montado é a hora da votação. Só pode votar quem nao participou do Draft, adicione ao contador do Draft cada vez que o mesmo receber um voto e depois que todos votarem, o jogador com mais votos ganha.
             </li>
             <li>
-              -&gt; Importante: O voto é feito no Draft como um todo, não em um item específico.
+              Importante: O voto é feito no Draft como um todo, não em um item específico.
             </li>
             <li>
-              4. O nome de cada participante é "Jogador" para manter o anonimato e evitar votos tendenciosos.
+              O nome de cada participante é "Jogador" para manter o anonimato e evitar votos tendenciosos.
             </li>
             <li>
-              -&gt; Importante: Bom jogo e DIVIRTA-SE.
+              Importante: Bom jogo e DIVIRTA-SE.
             </li>
           </ul>
         </div>
